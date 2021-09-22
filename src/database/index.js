04e258c3a0e388963a43629d01b14b724885d332
Carlos Castro <82064665+carlos-decastro@users.noreply.github.com>
2021-09-21 22:34:34 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 
 const client = new Client({
   host: 'localhost',
-  port: 5422,
+  port: 5422, // Alterar a porta de 5422 para 5432 caso for utilizar o PostgreSQL diretamente em vez do Docker.
   user: 'root',
   password: 'root',
   database: 'api',
